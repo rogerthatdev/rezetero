5d0d589cd29b30cab92f1f0359488920ed64a785
Roger Martinez <roger.that.dev@gmail.com>
2025-02-22 01:10:04 -0800

    Add dinosaur API endpoints and load data from JSON from deno express sample

--- main.ts
+++ main.ts
@@ -1,10 +1,29 @@
 // @ts-types="npm:@types/express@4.17.15"
 import express from "npm:express@4.18.2";
+import data from "./data.json" with { type: "json"}
 
 const app = express();
+let _ = data
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Dinosaur API!");
+});
+
+app.get("/api", (req, res) => {
+  res.send(data);
+});
+
+app.get("/api/:dinosaur", (req, res) => {
+  if (req?.params?.dinosaur) {
+    const found = data.find((item) =>
+      item.name.toLowerCase() === req.params.dinosaur.toLowerCase()
+    );
+    if (found) {
+      res.send(found);
+    } else {
+      res.send("No dinosaurs found.");
+    }
+  }
 });
 
 app.listen(8000);
